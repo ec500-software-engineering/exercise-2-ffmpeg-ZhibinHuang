52b1e74c5ba72a90772b88534983c350ea139cd5
ZhibinHuang <43126280+ZhibinHuang@users.noreply.github.com>
2019-03-02 20:05:15 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ex2',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Zhibin Huang',
+    author_email='hzhibin@bu.edu',
+    install_requires=["flake8", "pytest"],
+    description='Transfer video inot 480p and 720p'
+)
