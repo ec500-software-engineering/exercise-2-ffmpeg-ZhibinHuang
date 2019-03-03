c3ff0aac490a63e17acca8b9a0b724dc1a899193
ZhibinHuang <43126280+ZhibinHuang@users.noreply.github.com>
2019-03-02 21:26:56 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import subprocess
 import json
 from pathlib import Path
 
-def ffprobe(filename:Path) -> dict:
+def ffprobe_b(filename:Path) -> dict:
     meta = subprocess.check_output(['ffprobe', '-v', 'warning',
                                         '-print_format', 'json',
                                         '-show_streams',
@@ -18,14 +18,15 @@ def test_time():
     Q = queue.Queue()
     fin = './in/in1.mp4'
     fout = './out/in1.mp4_480p.mp4'
-    Q.put(fin)
+    name = 'in1.mp4'
+    Q.put(name)
     sun.convert_video(Q)
-    orig_meta = ffprobe(fin)
+    orig_meta = ffprobe_b(fin)
     orig_duration = float(orig_meta['streams'][0]['duration'])
 
-    meta_480 = ffprobe(fout)
+    meta_480 = ffprobe_b(fout)
     duration_480 = float(meta_480['streams'][0]['duration'])
-    assert orig_duration == isclose(duration_480)
+    assert orig_duration == isclose(orig_duration, duration_480, abs_tol=1)
 
 
 
