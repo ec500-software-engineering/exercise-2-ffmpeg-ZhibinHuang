f269b3b0db744da7c95ad40a7f0369312cb29356
ZhibinHuang <43126280+ZhibinHuang@users.noreply.github.com>
2019-03-03 18:00:41 -0500

    Update test_time.py

--- test_time.py
+++ test_time.py
@@ -18,17 +18,20 @@ def test_time():
     Q = queue.Queue()
     fin = './in/in1.mp4'
     fout = './out/in1.mp4_480p.mp4'
+    fout_720 = './out/in1.mp4_720p.mp4'
     name = 'in1.mp4'
     Q.put(name)
     sun.convert_video(Q)
     orig_meta = ffprobe_b(fin)
     orig_duration = float(orig_meta['streams'][0]['duration'])
-    print(orig_duration)
 
     meta_480 = ffprobe_b(fout)
     duration_480 = float(meta_480['streams'][0]['duration'])
-    print(duration_480)
+    meta_720 = ffprobe_b(fout_720)
+    duration_720 = float(meta_720['streams'][0]['duration'])
+    
     assert orig_duration== approx(duration_480, rel=0.01)
+    assert orig_duration== approx(duration_720, rel=0.01)
 
 
 if __name__ == '__main__':
